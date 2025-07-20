51ff863f5b9db50076dfe670a6a5d6736c88adb5
SUGAN-2007 <developer.sugan@gmail.com>
2025-07-20 11:41:26 +0530

    after for

--- main.js
+++ main.js
@@ -1,135 +1,126 @@
-const prompt = require("prompt-sync")()
+const prompt = require("prompt-sync")();
 
-const rows = 3
-const col = 3
+const rows = 3;
+const col = 3;
 
 const symbols = {
-    "A": 2,
-    "B": 4,
-    "C": 6,
-    "D": 8
-}
+  A: 2,
+  B: 4,
+  C: 6,
+  D: 8,
+};
 
 const sym_value = {
-    "A": 5,
-    "B": 4,
-    "C": 3,
-    "D": 2
-}
+  A: 5,
+  B: 4,
+  C: 3,
+  D: 2,
+};
 
 const des = () => {
-    while(true){
+  while (true) {
+    const deposit = prompt("enter the deposit: $");
+    const numdes = parseFloat(deposit);
 
-    const deposit = prompt("enter the deposit: $") 
-    const numdes = parseFloat(deposit)
-
-    if(isNaN(numdes) || numdes<=0){
-        console.log("invalid deposit")
+    if (isNaN(numdes) || numdes <= 0) {
+      console.log("invalid deposit");
+    } else {
+      return numdes;
     }
-    else{
-        return numdes
+  }
+};
+const line = () => {
+  while (true) {
+    const lines = prompt("enter the no of lines: ");
+    const numoflines = Number(lines);
+    if (isNaN(numoflines) || numoflines <= 0 || numoflines > 3) {
+      console.log("invalid no of lines");
+    } else {
+      return numoflines;
     }
-}
-} 
-const line = () =>{
-    while(true){
+  }
+};
 
-    const lines = prompt("enter the no of lines: ") 
-    const numoflines = Number(lines)
-    if(isNaN(numoflines) || numoflines<=0 || numoflines > 3)  {
-        console.log("invalid no of lines")
+const bet = (bal, lines) => {
+  while (true) {
+    const bet = prompt("enter the bet for per no of lines: ");
+    const bets = parseFloat(bet);
+    if (isNaN(bets) || bets <= 0 || bets > bal / lines) {
+      console.log("invalid bets");
+    } else {
+      return bets;
     }
-    else{
-        return numoflines
+  }
+};
+const spin = () => {
+  const sym = [];
+  for (const [symbol, count] of Object.entries(symbols)) {
+    for (let i = 0; i < count; i++) {
+      sym.push(symbol);
     }
-}
-}
-
-const bet = (bal , lines) => {
-    while(true){
-
-    const bet = prompt("enter the bet for per no of lines: ") 
-    const bets = parseFloat(bet)
-    if (isNaN(bets) || bets<=0 || bets  > (bal / lines)){
-             console.log("invalid bets")
+  }
+  const reel = [];
+  for (let i = 0; i < col; i++) {
+    reel.push([]);
+    const reelsym = [...sym];
+    for (let j = 0; j < rows; j++) {
+      let randomin = Math.floor(Math.random() * reelsym.length);
+      let selected = reelsym[randomin];
+      reel[i].push(selected);
+      reelsym.splice(randomin, 1);
     }
-    else{
-        return bets
+  }
+  return reel;
+};
+
+const transpose = (reel) => {
+  transreel = [];
+  for (i = 0; i < rows; i++) {
+    transreel.push([]);
+    for (j = 0; j < col; j++) {
+      transreel[i].push(reel[j][i]);
     }
+  }
+  return transreel;
+};
+
+const printrow = (rows) => {
+  for (const row of rows) {
+    let rowstr = "";
+    for (const [i, value] of row.entries()) {
+      rowstr += value;
+      if (i != row.length - 1) {
+        rowstr += " | ";
+      }
     }
-}
- const spin = () => {
-    const sym = []
-    for (const [symbol , count] of Object.entries(symbols)) {
-        for (let i = 0 ; i < count ; i++ ){
-            sym.push(symbol)
-        } 
+    console.log(rowstr);
+  }
+};
+
+const getwin = (row, bet, line) => {
+  let winning = 0;
+  for (let rows = 0; rows < line; rows++) {
+    const symbols = row[rows];
+    let alltrue = true;
+    for (const symbol of symbols) {
+      if (symbol != symbols[0]) {
+        alltrue = false;
+        break;
+      }
     }
-    const reel = []
-    for (let i = 0 ; i < col ; i++){
-        reel.push([])
-        const reelsym = [...sym]
-        for (let j = 0 ; j < rows ; j++ )
-        {
-            let randomin = Math. floor(Math.random() *  reelsym.length)
-            let selected = reelsym[randomin]
-            reel[i].push(selected)
-            reelsym.splice(randomin, 1)
-        }
+    if (alltrue) {
+      winning += bet * sym_value[symbol[0]];
     }
-    return reel
- }
+  }
 
- const transpose = (reel) => {
-        transreel = []
-        for ( i = 0 ; i < rows ; i++) {
-            transreel.push([])
-            for (j=0 ; j < col ; j++)
-            {
-                transreel [ i ] .push(reel [j][i])
-            }
-        }
-        return transreel
- }
+  return winning;
+};
 
- const printrow= (rows) => {
-        for (const row of rows){
-            let rowstr = ""
-            for ( const [ i , value] of row.entries()) {
-                rowstr += value
-                if ( i != row.length-1){
-                    rowstr += " | "
-                }
-            }
-            console.log(rowstr)
-        }
- }
-
- const getwin = (row,bet,line) => {
-        let winning = 0
-        for (let rows = 0 ; rows < line ; rows ++) {
-            const symbols = row[rows]
-            let alltrue = true
-            for (const symbol of symbols) {
-                if ( symbol != symbols[0])
-                {
-                    alltrue = false
-                    break
-                }
-            }
-            if (alltrue){
-                winning += bet * sym_value[symbol[0]]
-            }
-        }
-
-        return winning
- }
-
- let bal = des()
- const lines = line()
- const bets=bet(bal,lines)
- const reels = spin()
- const tran = transpose(reels)
- printrow(tran)
- const win = getwin(tran , bets , lines)
- console.log ("You won : $",win)
+let bal = des();
+const lines = line();
+const bets = bet(bal, lines);
+const reels = spin();
+const tran = transpose(reels);
+printrow(tran);
+const win = getwin(tran, bets, lines);
+console.log("You won : $", win);
